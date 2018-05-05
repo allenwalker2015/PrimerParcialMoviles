5b5eeebd81ef9d4c266a70490ece19d43c6aa954
allen <allen>
2018-05-05 11:50:20 -0600

    Desde la EditContact Activity ya almacena n numeros de telefono y n emails.

--- app/src/main/java/com/allen/primerparcialmoviles/EditContact.java
+++ app/src/main/java/com/allen/primerparcialmoviles/EditContact.java
@@ -1,19 +1,21 @@
 package com.allen.primerparcialmoviles;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -32,34 +34,45 @@ import java.util.LinkedHashMap;
 
 public class EditContact extends AppCompatActivity {
     public static final int PICK_IMAGE = 1;
+
+    //Elementos de la view
+    EditText given_name, family_name, direction,birthday,new_mail,new_phone;
+    com.mikhaellopez.circularimageview.CircularImageView image;
+    Button add_button,add_email_button;
+    Spinner type;
+    FloatingActionButton fab;
+
+    //Variables necesarias para recursos que son dinamicos
     LinkedHashMap<String,String> phones;
     ArrayList<String> emails;
+    String picture;
 
-    EditText nombre,apellido,direccion,birthday,new_mail;
-    Spinner type;
-    RecyclerView phonesrv,emailrv;
-    Button add_button,add_email_button;
+    //Adaptadores para sus respectivos recycler
     PhoneEditAdapter pa;
     MailEditAdapter ma;
-    String picture;
-    EditText new_phone;
-    com.mikhaellopez.circularimageview.CircularImageView image;
+    //Recycler views para el listado de telefonos y de emails
+    RecyclerView phones_recycler, email_recycler;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact);
-        nombre = findViewById(R.id.edit_text_name);
+        //Se buscan los elementos en la view
+        given_name = findViewById(R.id.edit_text_name);
         type = findViewById(R.id.phone_type_spinner);
-        apellido = findViewById(R.id.edit_text_last_name);
-        direccion = findViewById(R.id.edit_text_address);
+        family_name = findViewById(R.id.edit_text_last_name);
+        direction = findViewById(R.id.edit_text_address);
         image = findViewById(R.id.contact_picture);
         birthday = findViewById(R.id.edit_text_birthday);
-        phonesrv = findViewById(R.id.recycler_phone);
-        emailrv = findViewById(R.id.recycler_email);
+        phones_recycler = findViewById(R.id.recycler_phone);
+        email_recycler = findViewById(R.id.recycler_email);
         add_button = findViewById(R.id.add_new_contact_button);
         add_email_button = findViewById(R.id.add_new_email_button);
         new_phone = findViewById(R.id.new_phone_number);
         new_mail = findViewById(R.id.new_email);
+
+
         if(phones==null){
             phones = new LinkedHashMap<>();
 
@@ -70,24 +83,50 @@ public class EditContact extends AppCompatActivity {
 
         }
 
+        confAdapters();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fab =  findViewById(R.id.fab);
+        confListeners();
+
+    }
+
+//    @Override
+//    public void onSaveInstanceState(Bundle savedInstanceState) {
+//                savedInstanceState.putParcelable("phones_recycler_state", phones_recycler.getLayoutManager().onSaveInstanceState());
+//                // Se guardan los items
+//                savedInstanceState.putSerializable("contact_list", pa.getList());
+//                //savedInstanceState.putSerializable("contact_adapter",ca);
+//                // if(fca!=null) {
+//
+//                //    savedInstanceState.putSerializable("filtered_adapter", fca);
+//                //}
+//            }
+//        }
+//        super.onSaveInstanceState(savedInstanceState);
+//    }
+//
+//    public void restorePreviousState(Bundle savedInstanceState) {
+//
 
 
+//    }
+    // Configura y setea los adapters a sus respectivos elementos.
+    public void confAdapters(){
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.phone_types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         type.setAdapter(adapter);
-
-
         pa = new PhoneEditAdapter(this,phones);
-        phonesrv.setLayoutManager(new LinearLayoutManager(this));
-        phonesrv.setAdapter(pa);
+        phones_recycler.setLayoutManager(new LinearLayoutManager(this));
+        phones_recycler.setAdapter(pa);
 
         ma = new MailEditAdapter(this,emails);
-        emailrv.setLayoutManager(new LinearLayoutManager(this));
-        emailrv.setAdapter(ma);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        email_recycler.setLayoutManager(new LinearLayoutManager(this));
+        email_recycler.setAdapter(ma);
+    }
+    public void confListeners(){
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -96,13 +135,13 @@ public class EditContact extends AppCompatActivity {
                         .setAction("Action", null).show();
                 String id = "0";
                 ArrayList<String> name = new ArrayList<>();
-                name.add(nombre.getText().toString() +" "+ apellido.getText().toString());
-                name.add(nombre.getText().toString());
-                name.add(apellido.getText().toString());
+                name.add(given_name.getText().toString() +" "+ family_name.getText().toString());
+                name.add(given_name.getText().toString());
+                name.add(family_name.getText().toString());
                 //LinkedHashMap<String,String> numbers = new LinkedHashMap<String,String>();
                 ArrayList<String>  emails = new ArrayList<String>();
                 //emails.add(email.getText().toString());
-                String address = direccion.getText().toString();
+                String address = direction.getText().toString();
                 Boolean favorite = false;
 
                 Contact c = new Contact(id,name,pa.getList(),ma.getList(),address,picture,favorite,new Date());
@@ -147,14 +186,11 @@ public class EditContact extends AppCompatActivity {
                 int anio = fechaActual.get(Calendar.YEAR);
                 int mes = fechaActual.get(Calendar.MONTH);
                 int dia = fechaActual.get(Calendar.DAY_OF_MONTH);
-                //DatePickerFragment datePickerFragment = new DatePickerFragment();
-                //datePickerFragment.show(getSupportFragmentManager(), "datePicker");
                 DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear,
                                           int dayOfMonth) {
-                        // TODO Auto-generated method stub
                         birthday.setText(dayOfMonth + "/" + monthOfYear + "/" + year);
                     }
 
@@ -163,7 +199,6 @@ public class EditContact extends AppCompatActivity {
                 picker.show();
             }
         });
-
     }
 
     @Override
@@ -176,5 +211,7 @@ public class EditContact extends AppCompatActivity {
             Toast.makeText(this, "URI:"+selectedImageURI.toString(), Toast.LENGTH_SHORT).show();
         }
     }
+
+    
 
 }
